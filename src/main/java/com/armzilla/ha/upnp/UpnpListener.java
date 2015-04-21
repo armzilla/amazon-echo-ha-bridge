659cfd3e438b594e91a37651695a8b5aa4dfd172
arm <armzilla@gmail.com>
2015-04-20 21:16:58 -0500

    shutdown instance if upnp is borked

--- src/main/java/com/armzilla/ha/upnp/UpnpListener.java
+++ src/main/java/com/armzilla/ha/upnp/UpnpListener.java
@@ -1,7 +1,10 @@
 package com.armzilla.ha.upnp;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -26,6 +29,9 @@ public class UpnpListener {
     @Value("${upnp.config.address}")
     private String responseAddress;
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Scheduled(fixedDelay = Integer.MAX_VALUE)
     public void startListening(){
         log.info("Starting UPNP Discovery Listener");
@@ -42,48 +48,44 @@ public class UpnpListener {
                 upnpMulticastSocket.receive(packet);
                 String packetString = new String(packet.getData());
                 if(isSSDPDiscovery(packetString)){
-                    log.info("Got SSDP Discovery packet from " +  packet.getAddress().getHostAddress() + ":" + packet.getPort());
+                    log.debug("Got SSDP Discovery packet from " + packet.getAddress().getHostAddress() + ":" + packet.getPort());
                     sendUpnpResponse(responseSocket, packet.getAddress(), packet.getPort());
                 }
             }
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        } catch (SocketException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+        }  catch (IOException e) {
+            log.error("UpnpListener encountered an error. Shutting down", e);
+            ConfigurableApplicationContext context = (ConfigurableApplicationContext) UpnpListener.this.applicationContext;
+            context.close();
+
         }
         log.info("UPNP Discovery Listener Stopped");
 
     }
 
+    /**
+     * very naive ssdp discovery packet detection
+     * @param body
+     * @return
+     */
     protected boolean isSSDPDiscovery(String body){
-        if(body.startsWith("M-SEARCH * HTTP/1.1") && body.contains("MAN: \"ssdp:discover\"")){
+        if(body != null && body.startsWith("M-SEARCH * HTTP/1.1") && body.contains("MAN: \"ssdp:discover\"")){
             return true;
         }
         return false;
     }
 
     String discoveryTemplate = "HTTP/1.1 200 OK\r\n" +
-            "CACHE-CONTROL: max-age=86400\r\n" +
-            "DATE: Thu, 09 Apr 2015 19:49:04 GMT\r\n" +
-            "EXT:\r\n" +
-            "LOCATION: http://%s:%s/upnp/cheese/setup.xml\r\n" +
+            "LOCATION: http://%s:%s/upnp/amazon-ha-bridge/setup.xml\r\n" +
             "OPT: \"http://schemas.upnp.org/upnp/1/0/\"; ns=01\r\n" +
             "01-NLS: 86f81bce-1dd2-11b2-a14f-ab8000ac1b49\r\n" +
             "SERVER: Unspecified, UPnP/1.0, Unspecified\r\n" +
             "X-User-Agent: redsonic\r\n" +
             "ST: urn:Belkin:device:**\r\n" +
             "USN: uuid:Socket-1_0-221438K0100073::urn:Belkin:device:**\r\n\r\n";
-    protected boolean sendUpnpResponse(DatagramSocket socket, InetAddress requester, int sourcePort){
+    protected void sendUpnpResponse(DatagramSocket socket, InetAddress requester, int sourcePort) throws IOException {
         String discoveryResponse = String.format(discoveryTemplate, responseAddress, httpServerPort);
         DatagramPacket response = new DatagramPacket(discoveryResponse.getBytes(), discoveryResponse.length(), requester, sourcePort);
-        try {
-            socket.send(response);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        socket.send(response);
 
-        return false;
     }
 }
