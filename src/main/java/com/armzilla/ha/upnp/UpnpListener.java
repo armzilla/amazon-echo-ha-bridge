65073931e960533791cd211784b70c17c3770f8e
markfalk <mark@markfalk.com>
2015-05-17 02:19:15 -0700

    Added proper handling for servers with multiple NICs

--- src/main/java/com/armzilla/ha/upnp/UpnpListener.java
+++ src/main/java/com/armzilla/ha/upnp/UpnpListener.java
@@ -11,6 +11,9 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 import java.net.*;
 
+import java.util.Enumeration;
+import org.apache.http.conn.util.*;
+
 /**
  * Created by arm on 4/11/15.
  */
@@ -34,31 +37,54 @@ public class UpnpListener {
 
     @Scheduled(fixedDelay = Integer.MAX_VALUE)
     public void startListening(){
-        log.info("Starting UPNP Discovery Listener");
+    	log.info("Starting UPNP Discovery Listener");
 
-        try (DatagramSocket responseSocket = new DatagramSocket(upnpResponsePort);
-             MulticastSocket upnpMulticastSocket  = new MulticastSocket(UPNP_DISCOVERY_PORT)) {
+    	try (DatagramSocket responseSocket = new DatagramSocket(upnpResponsePort);
+    			MulticastSocket upnpMulticastSocket  = new MulticastSocket(UPNP_DISCOVERY_PORT);) {
+    		InetSocketAddress socketAddress = new InetSocketAddress(UPNP_MULTICAST_ADDRESS, UPNP_DISCOVERY_PORT);
 
-            InetAddress upnpGroupAddress = InetAddress.getByName(UPNP_MULTICAST_ADDRESS);
-            upnpMulticastSocket.joinGroup(upnpGroupAddress);
 
-            while(true){ //trigger shutdown here
-                byte[] buf = new byte[1024];
-                DatagramPacket packet = new DatagramPacket(buf, buf.length);
-                upnpMulticastSocket.receive(packet);
-                String packetString = new String(packet.getData());
-                if(isSSDPDiscovery(packetString)){
-                    log.debug("Got SSDP Discovery packet from " + packet.getAddress().getHostAddress() + ":" + packet.getPort());
-                    sendUpnpResponse(responseSocket, packet.getAddress(), packet.getPort());
-                }
-            }
-        }  catch (IOException e) {
-            log.error("UpnpListener encountered an error. Shutting down", e);
-            ConfigurableApplicationContext context = (ConfigurableApplicationContext) UpnpListener.this.applicationContext;
-            context.close();
 
-        }
-        log.info("UPNP Discovery Listener Stopped");
+    		Enumeration<NetworkInterface> ifs =
+    				NetworkInterface.getNetworkInterfaces();
+    		while (ifs.hasMoreElements()) {
+    			NetworkInterface xface = ifs.nextElement();
+    			Enumeration<InetAddress> addrs = xface.getInetAddresses();
+    			String name = xface.getName();
+    			int IPsPerNic = 0;
+
+    			while (addrs.hasMoreElements()) {
+    				InetAddress addr = addrs.nextElement();
+    				log.debug(name + " ... has addr " + addr);
+    				if (InetAddressUtils.isIPv4Address(addr.getHostAddress())) {
+    					IPsPerNic++;
+    				}
+    			}
+    			log.debug("Checking " + name + " to our interface set");
+    			if (IPsPerNic > 0) {
+    				upnpMulticastSocket.joinGroup(socketAddress, xface);
+    				log.debug("Adding " + name + " to our interface set");
+    			}
+    		}
+
+    		while(true){ //trigger shutdown here
+    			byte[] buf = new byte[1024];
+    			DatagramPacket packet = new DatagramPacket(buf, buf.length);
+    			upnpMulticastSocket.receive(packet);
+    			String packetString = new String(packet.getData());
+    			if(isSSDPDiscovery(packetString)){
+    				log.debug("Got SSDP Discovery packet from " + packet.getAddress().getHostAddress() + ":" + packet.getPort());
+    				sendUpnpResponse(responseSocket, packet.getAddress(), packet.getPort());
+    			}
+    		}
+
+    	}  catch (IOException e) {
+    		log.error("UpnpListener encountered an error. Shutting down", e);
+    		ConfigurableApplicationContext context = (ConfigurableApplicationContext) UpnpListener.this.applicationContext;
+    		context.close();
+
+    	}
+    	log.info("UPNP Discovery Listener Stopped");
 
     }
 
