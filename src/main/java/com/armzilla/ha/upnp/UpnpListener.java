acc303f3c581bf3d88153441d39ab70568662b8a
markfalk <mark@markfalk.com>
2015-05-17 03:48:25 -0700

    Multiple NIC support added

--- src/main/java/com/armzilla/ha/upnp/UpnpListener.java
+++ src/main/java/com/armzilla/ha/upnp/UpnpListener.java
@@ -19,26 +19,26 @@ import org.apache.http.conn.util.*;
  */
 @Component
 public class UpnpListener {
-    private Logger log = Logger.getLogger(UpnpListener.class);
-    private static final int UPNP_DISCOVERY_PORT = 1900;
-    private static final String UPNP_MULTICAST_ADDRESS = "239.255.255.250";
+	private Logger log = Logger.getLogger(UpnpListener.class);
+	private static final int UPNP_DISCOVERY_PORT = 1900;
+	private static final String UPNP_MULTICAST_ADDRESS = "239.255.255.250";
 
-    @Value("${upnp.response.port}")
-    private int upnpResponsePort;
+	@Value("${upnp.response.port}")
+	private int upnpResponsePort;
 
-    @Value("${server.port}")
-    private int httpServerPort;
+	@Value("${server.port}")
+	private int httpServerPort;
 
-    @Value("${upnp.config.address}")
-    private String responseAddress;
+	@Value("${upnp.config.address}")
+	private String responseAddress;
 
-    @Autowired
-    private ApplicationContext applicationContext;
+	@Autowired
+	private ApplicationContext applicationContext;
 
-    @Scheduled(fixedDelay = Integer.MAX_VALUE)
+	@Scheduled(fixedDelay = Integer.MAX_VALUE)
 	public void startListening(){
 		log.info("Starting UPNP Discovery Listener");
-	
+
 		try (DatagramSocket responseSocket = new DatagramSocket(upnpResponsePort);
 				MulticastSocket upnpMulticastSocket  = new MulticastSocket(UPNP_DISCOVERY_PORT);) {
 			InetSocketAddress socketAddress = new InetSocketAddress(UPNP_MULTICAST_ADDRESS, UPNP_DISCOVERY_PORT);
@@ -49,7 +49,7 @@ public class UpnpListener {
 				Enumeration<InetAddress> addrs = xface.getInetAddresses();
 				String name = xface.getName();
 				int IPsPerNic = 0;
-	
+
 				while (addrs.hasMoreElements()) {
 					InetAddress addr = addrs.nextElement();
 					log.debug(name + " ... has addr " + addr);
@@ -63,7 +63,7 @@ public class UpnpListener {
 					log.debug("Adding " + name + " to our interface set");
 				}
 			}
-	
+
 			while(true){ //trigger shutdown here
 				byte[] buf = new byte[1024];
 				DatagramPacket packet = new DatagramPacket(buf, buf.length);
@@ -74,44 +74,44 @@ public class UpnpListener {
 					sendUpnpResponse(responseSocket, packet.getAddress(), packet.getPort());
 				}
 			}
-	
+
 		}  catch (IOException e) {
 			log.error("UpnpListener encountered an error. Shutting down", e);
 			ConfigurableApplicationContext context = (ConfigurableApplicationContext) UpnpListener.this.applicationContext;
 			context.close();
-	
+
 		}
 		log.info("UPNP Discovery Listener Stopped");
-	
+
 	}
 
-    /**
-     * very naive ssdp discovery packet detection
-     * @param body
-     * @return
-     */
-    protected boolean isSSDPDiscovery(String body){
-        if(body != null && body.startsWith("M-SEARCH * HTTP/1.1") && body.contains("MAN: \"ssdp:discover\"")){
-            return true;
-        }
-        return false;
-    }
+	/**
+	 * very naive ssdp discovery packet detection
+	 * @param body
+	 * @return
+	 */
+	protected boolean isSSDPDiscovery(String body){
+		if(body != null && body.startsWith("M-SEARCH * HTTP/1.1") && body.contains("MAN: \"ssdp:discover\"")){
+			return true;
+		}
+		return false;
+	}
 
-    String discoveryTemplate = "HTTP/1.1 200 OK\r\n" +
-            "CACHE-CONTROL: max-age=86400\r\n" +
-            "EXT:\r\n" +
-            "LOCATION: http://%s:%s/upnp/amazon-ha-bridge/setup.xml\r\n" +
-            "OPT: \"http://schemas.upnp.org/upnp/1/0/\"; ns=01\r\n" +
-            "01-NLS: %s\r\n" +
-            "ST: urn:Belkin:device:**\r\n" +
-            "USN: uuid:Socket-1_0-221438K0100073::urn:Belkin:device:**\r\n\r\n";
-    protected void sendUpnpResponse(DatagramSocket socket, InetAddress requester, int sourcePort) throws IOException {
-        String discoveryResponse = String.format(discoveryTemplate, responseAddress, httpServerPort, getRandomUUIDString());
-        DatagramPacket response = new DatagramPacket(discoveryResponse.getBytes(), discoveryResponse.length(), requester, sourcePort);
-        socket.send(response);
-    }
+	String discoveryTemplate = "HTTP/1.1 200 OK\r\n" +
+			"CACHE-CONTROL: max-age=86400\r\n" +
+			"EXT:\r\n" +
+			"LOCATION: http://%s:%s/upnp/amazon-ha-bridge/setup.xml\r\n" +
+			"OPT: \"http://schemas.upnp.org/upnp/1/0/\"; ns=01\r\n" +
+			"01-NLS: %s\r\n" +
+			"ST: urn:Belkin:device:**\r\n" +
+			"USN: uuid:Socket-1_0-221438K0100073::urn:Belkin:device:**\r\n\r\n";
+	protected void sendUpnpResponse(DatagramSocket socket, InetAddress requester, int sourcePort) throws IOException {
+		String discoveryResponse = String.format(discoveryTemplate, responseAddress, httpServerPort, getRandomUUIDString());
+		DatagramPacket response = new DatagramPacket(discoveryResponse.getBytes(), discoveryResponse.length(), requester, sourcePort);
+		socket.send(response);
+	}
 
-    protected String getRandomUUIDString(){
-        return "88f6698f-2c83-4393-bd03-cd54a9f8595"; // https://xkcd.com/221/
-    }
+	protected String getRandomUUIDString(){
+		return "88f6698f-2c83-4393-bd03-cd54a9f8595"; // https://xkcd.com/221/
+	}
 }
