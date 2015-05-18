6e76d73126b3bffa632f3f5b74e6ee92f9f2e693
oleg <olegk@hybridsoftware.com>
2015-05-18 10:30:12 -0400

    change page size from 10 to 100 to allow bridge to handle more than 10 devices

--- src/main/java/com/armzilla/ha/dao/DeviceRepository.java
+++ src/main/java/com/armzilla/ha/dao/DeviceRepository.java
@@ -1,5 +1,8 @@
 package com.armzilla.ha.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 import java.util.List;
@@ -8,7 +11,7 @@ import java.util.List;
  * Created by arm on 4/13/15.
  */
 public interface DeviceRepository extends ElasticsearchRepository<DeviceDescriptor, String> {
-    List<DeviceDescriptor> findByDeviceType(String type);
+    Page<DeviceDescriptor> findByDeviceType(String type, Pageable request);
     List<DeviceDescriptor> findAll();
     DeviceDescriptor findOne(String id);
 
