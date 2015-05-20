60d79a811d9abdec0e2195b4a45ae2245b1c6530
Ronald Gouldner <gouldner@hawaii.edu>
2015-05-19 22:21:05 -1000

    fixed start up shell

--- startEchoStBridge.sh
+++ startEchoStBridge.sh
@@ -1,11 +1,16 @@
-upnp.response.port=50000
-upnp.config.address={Enter your local ip here}
-server.port=8080
-application.smartthings.app.clientId={Enter your smart things app client id here}
-application.smartthings.app.clientSecret={Enter your smart things app client secret here}
-application.smartthings.app.externalIP={enter your router external ip here}
+#
+# Replace all values with {} below rmoving the curly braces
+#
+export LOCAL_IP={Enter this servers local ip}
+
+export UPNP_PORT=50000
+export SERVER_PORT=8080
+export SMART_APP_CLIENTID={enter smart app client id}
+export SMART_APP_SECRET={enter smar app secret}
+# Note in brower enter "what is my ip" to find this value
+export ROUTER_EXTERNAL_IP={enter external ip of router}
 
 # Note: You need to port forward "server.port" to your local ip "upnp.config.address"
 #       on your router with external ip "application.smartthings.app.externalIP"
 #
-java -jar target/amazon-echo-bridge-0.1.2.jar --upnp.response.port=${upnp.response.port} --server.port=${server.port} --upnp.config.address=${upnp.config.address} --application.smartthings.app.clientId={application.smartthings.app.clientId} --application.smartthings.app.clientSecret=${application.smartthings.app.clientSecret} --application.smartthings.app.externalIP=${application.smartthings.app.externalIP}
+java -jar target/amazon-echo-bridge-0.1.2.jar --upnp.response.port=${UPNP_PORT} --server.port=${SERVER_PORT} --upnp.config.address=${LOCAL_IP} --application.smartthings.app.clientId=${SMART_APP_CLIENTID} --application.smartthings.app.clientSecret=${SMART_APP_SECRET} --application.smartthings.app.externalIP=${ROUTER_EXTERNAL_IP}
