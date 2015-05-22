c1352df3ba678cd8eaf310bf73f4b267b3d2b470
Ronald Gouldner <gouldner@hawaii.edu>
2015-05-22 08:40:18 +0000

    More cleanup of startup shell

--- startEchoStBridge.sh
+++ startEchoStBridge.sh
@@ -12,9 +12,11 @@ export SMART_APP_SECRET={enter smar app secret}
 # Note in brower enter "what is my ip" to find this value
 export ROUTER_EXTERNAL_IP={enter external ip of router}
 
+mkdir -p logs
+
 # Note: You need to port forward "server.port" to your local ip "upnp.config.address"
 #       on your router with external ip "application.smartthings.app.externalIP"
 #
-java -jar amazon-echo-bridge-smart-things-0.1.2.1.jar --upnp.response.port=${UPNP_PORT} --server.port=${SERVER_PORT} --upnp.config.address=${LOCAL_IP} --application.smartthings.app.clientId=${SMART_APP_CLIENTID} --application.smartthings.app.clientSecret=${SMART_APP_SECRET} --application.smartthings.app.externalIP=${ROUTER_EXTERNAL_IP} > logs/echobridge.log 2>&1 &
-echo "$!" > echoBridge.pid
+java -jar amazon-echo-bridge-smart-things-0.1.2.1.jar --upnp.response.port=${UPNP_PORT} --server.port=${SERVER_PORT} --upnp.config.address=${LOCAL_IP} --application.smartthings.app.clientId=${SMART_APP_CLIENTID} --application.smartthings.app.clientSecret=${SMART_APP_SECRET} --application.smartthings.app.externalIP=${ROUTER_EXTERNAL_IP} > logs/echoBridge.log 2>&1 &
+echo "$!" > logs/echoBridge.pid
 
