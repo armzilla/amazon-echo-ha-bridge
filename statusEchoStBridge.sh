24f2bbaf86c92cd5faee4de92edc35da472bce8c
Ronald Gouldner <gouldner@hawaii.edu>
2015-05-28 01:32:11 +0000

    fixed the service install to run on raspberry as pi user This can be altered for other unix default users

--- statusEchoStBridge.sh
+++ statusEchoStBridge.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-statme=`cat logs/echoBridge.pid`
-running=`ps -p $statme | grep -v "TIME"`
-if [ "${running}" == "" ]
+if [ -f logs/echoBridge.pid ]
 then
-    echo Not Running
+    statme=`cat logs/echoBridge.pid`
+    running=`ps -p $statme | grep -v "TIME"`
+    if [ "${running}" == "" ]
+    then
+        echo Not Running
+    else
+        echo Running PID=${statme}
+    fi
 else
-    echo Running PID=${statme}
+    echo Not Running
 fi
-
